2cfa5994eb49ad437406d735c7a4d7530d3411ff
asmith <andrw.l.smth@gmail.com>
2017-10-26 02:09:47 -0400

    Make device listview expandable

--- PiHA-AndroidClient/app/src/main/java/com/andlee90/piha/piha_androidclient/UI/DeviceListFragment.java
+++ PiHA-AndroidClient/app/src/main/java/com/andlee90/piha/piha_androidclient/UI/DeviceListFragment.java
@@ -1,18 +1,15 @@
 package com.andlee90.piha.piha_androidclient.UI;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.andlee90.piha.piha_androidclient.R;
@@ -24,7 +21,6 @@ import DeviceObjects.Device;
 
 public class DeviceListFragment extends ListFragment
 {
-    private ListView mListView;
     private Context mContext;
     private DeviceListArrayAdapter mAdapter;
     private ArrayList<Device> mDevices = new ArrayList<>();
@@ -52,15 +48,40 @@ public class DeviceListFragment extends ListFragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_device_list, container, false);
-        mListView = view.findViewById(android.R.id.list);
-        TextView emptyText = view.findViewById(R.id.empty_listview);
-        mListView.setEmptyView(emptyText);
 
         mAdapter = new DeviceListArrayAdapter(mContext,
                 android.R.layout.simple_list_item_1, mDevices);
         setListAdapter(mAdapter);
 
         return view;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+
+        TextView itemTextView = v.findViewById(R.id.device_detail);
+        if(itemTextView.getVisibility() == View.GONE)
+        {
+            itemTextView.setVisibility(View.VISIBLE);
+            for(int i = 0; i < mAdapter.getCount(); i++)
+            {
+                View otherView = l.getChildAt(i);
+                if(otherView != null)
+                {
+                    TextView otherTextView = otherView.findViewById(R.id.device_detail);
+                    if(i != position)
+                    {
+                        otherTextView.setVisibility(View.GONE);
+                    }
+                }
+            }
+        }
+        else
+        {
+            itemTextView.setVisibility(View.GONE);
+        }
     }
 
     public void setListView(ArrayList<Device> devices)
@@ -90,12 +111,15 @@ public class DeviceListFragment extends ListFragment
 
             if (convertView == null)
             {
-                convertView = mInflater.inflate(R.layout.list_devices, parent, false);
+                convertView = mInflater.inflate(R.layout.list_device_items, parent, false);
                 convertView.setTag(new ViewHolder(convertView));
             }
 
             ViewHolder holder = (ViewHolder) convertView.getTag();
             holder.deviceName.setText(device.getDeviceName());
+            holder.deviceSwitch.setOnClickListener(view -> {
+
+            });
 
             return convertView;
         }
@@ -103,10 +127,14 @@ public class DeviceListFragment extends ListFragment
         private class ViewHolder
         {
             private final TextView deviceName;
+            private final TextView deviceDetail;
+            private final Switch deviceSwitch;
 
             ViewHolder(View view)
             {
-                deviceName = view.findViewById(R.id.deviceName);
+                deviceName = view.findViewById(R.id.device_name);
+                deviceDetail = view.findViewById(R.id.device_detail);
+                deviceSwitch = view.findViewById(R.id.device_switch);
             }
         }
     }
