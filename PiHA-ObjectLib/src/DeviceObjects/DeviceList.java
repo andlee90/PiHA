a26b1fc759f603c31a6fa36d85444a3e432e66d2
asmith <andrw.l.smth@gmail.com>
2017-10-28 19:37:12 -0400

    Add server id to devicelist

--- PiHA-ObjectLib/src/DeviceObjects/DeviceList.java
+++ PiHA-ObjectLib/src/DeviceObjects/DeviceList.java
@@ -8,16 +8,28 @@ import java.util.ArrayList;
  */
 public class DeviceList implements Serializable
 {
+    private int serverId;
     private ArrayList<Device> devices;
 
     public DeviceList()
     {
+        this.serverId = -1;
         this.devices = new ArrayList<>();
+    }
+
+    public int getServerId()
+    {
+        return this.serverId;
     }
 
     public ArrayList<Device> getDevices()
     {
         return devices;
+    }
+
+    public void setServerId(int id)
+    {
+        this.serverId = id;
     }
 
     public void setDevices(ArrayList<Device> d)
