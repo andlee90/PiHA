9becf97823e7d7e416a7f0e47fd76e1ba250afc5
asmith <andrw.l.smth@gmail.com>
2017-10-28 20:16:13 -0400

    Handle screen rotation for fragments in main activity via the solution described here: https://stackoverflow.com/questions/19393076/how-to-properly-handle-screen-rotation-with-a-viewpager-and-nested-fragments

--- PiHA-AndroidClient/app/src/main/java/com/andlee90/piha/piha_androidclient/UI/Controls/DeviceListPagerAdapter.java
+++ PiHA-AndroidClient/app/src/main/java/com/andlee90/piha/piha_androidclient/UI/Controls/DeviceListPagerAdapter.java
@@ -2,22 +2,43 @@ package com.andlee90.piha.piha_androidclient.UI.Controls;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.andlee90.piha.piha_androidclient.Database.ServerItem;
 
 import java.util.List;
 
-class DeviceListPagerAdapter extends FragmentPagerAdapter
+class DeviceListPagerAdapter extends PagerAdapter
 {
     private List<ServerItem> mServers;
     private List<Fragment> mFragments;
+    private FragmentManager mFragmentManager;
 
     DeviceListPagerAdapter(FragmentManager fm, List<ServerItem> s, List<Fragment> f)
     {
-        super(fm);
         this.mServers = s;
         this.mFragments = f;
+        this.mFragmentManager = fm;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object)
+    {
+        mFragmentManager.beginTransaction().remove(mFragments.get(position)).commit();
+        mFragments.set(position, null);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position)
+    {
+        Fragment fragment = getItem(position);
+        mFragmentManager.beginTransaction()
+                .add(container.getId(), fragment, "fragment:" + position)
+                .commit();
+
+        return fragment;
     }
 
     @Override
@@ -27,8 +48,17 @@ class DeviceListPagerAdapter extends FragmentPagerAdapter
     }
 
     @Override
+    public boolean isViewFromObject(View view, Object object)
+    {
+        return ((Fragment) object).getView() == view;
+    }
+
     public Fragment getItem(int position)
     {
+        if(mFragments.get(position) ==  null)
+        {
+            mFragments.set(position, WrapperFragment.newInstance());
+        }
         return mFragments.get(position);
     }
 
